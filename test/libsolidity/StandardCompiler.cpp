ce60a7aae7d39487fd5d73671e9a002696478d8e
Alexander Arlt <alexander.arlt@arlt-labs.com>
2024-02-23 21:38:50 +0100

    Revert some tests - we support now newlines again inside json.

--- test/libsolidity/StandardCompiler.cpp
+++ test/libsolidity/StandardCompiler.cpp
@@ -170,8 +170,6 @@ BOOST_AUTO_TEST_CASE(assume_object_input)
 	result = compile("");
 	BOOST_CHECK(containsError(result, "JSONError", "[json.exception.parse_error.101] parse error at line 1, column 1: attempting to parse an empty input; check that your input string or stream contains the expected JSON"));
 	result = compile("invalid");
-	std::cout << solidity::util::jsonPrettyPrint(result) << std::endl;
-
 	BOOST_CHECK(containsError(result, "JSONError", "[json.exception.parse_error.101] parse error at line 1, column 1: syntax error while parsing value - invalid literal; last read: 'i'"));
 	result = compile("\"invalid\"");
 	BOOST_CHECK(containsError(result, "JSONError", "Input is not a JSON object."));
@@ -1229,15 +1227,7 @@ BOOST_AUTO_TEST_CASE(metadata_without_compilation)
 {
 	// NOTE: the contract code here should fail to compile due to "out of stack"
 	// If the metadata is successfully returned, that means no compilation was attempted.
-	std::string contractCode = R"(
-contract A {
-  function x(uint a, uint b, uint c, uint d, uint e, uint f, uint g, uint h, uint i, uint j, uint k, uint l, uint m, uint n, uint o, uint p) pure public {}
-  function y() pure public {
-    uint a; uint b; uint c; uint d; uint e; uint f; uint g; uint h; uint i; uint j; uint k; uint l; uint m; uint n; uint o; uint p;
-    x(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
-  }
-})";
-	std::string input = R"(
+	char const* input = R"(
 	{
 		"language": "Solidity",
 		"settings": {
@@ -1247,7 +1237,13 @@ contract A {
 		},
 		"sources": {
 			"fileA": {
-				"content": )" + Json(contractCode).dump() + R"(
+				"content": "contract A {
+  function x(uint a, uint b, uint c, uint d, uint e, uint f, uint g, uint h, uint i, uint j, uint k, uint l, uint m, uint n, uint o, uint p) pure public {}
+  function y() pure public {
+    uint a; uint b; uint c; uint d; uint e; uint f; uint g; uint h; uint i; uint j; uint k; uint l; uint m; uint n; uint o; uint p;
+    x(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
+  }
+}"
 			}
 		}
 	}
@@ -1333,36 +1329,7 @@ BOOST_AUTO_TEST_CASE(use_stack_optimization)
 {
 	// NOTE: the contract code here should fail to compile due to "out of stack"
 	// If we enable stack optimization, though, it will compile.
-	std::string contractCode = R"(
-		contract A {
-			function y() public {
-				assembly {
-					function fun() -> a3, b3, c3, d3, e3, f3, g3, h3, i3, j3, k3, l3, m3, n3, o3, p3
-					{
-						let a := 1
-						let b := 1
-						let z3 := 1
-						sstore(a, b)
-						sstore(add(a, 1), b)
-						sstore(add(a, 2), b)
-						sstore(add(a, 3), b)
-						sstore(add(a, 4), b)
-						sstore(add(a, 5), b)
-						sstore(add(a, 6), b)
-						sstore(add(a, 7), b)
-						sstore(add(a, 8), b)
-						sstore(add(a, 9), b)
-						sstore(add(a, 10), b)
-						sstore(add(a, 11), b)
-						sstore(add(a, 12), b)
-					}
-					let a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1, m1, n1, o1, p1 := fun()
-					let a2, b2, c2, d2, e2, f2, g2, h2, i2, j2, k2, l2, m2, n2, o2, p2 := fun()
-					sstore(a1, a2)
-				}
-			}
-		})";
-	std::string input = R"(
+	char const* input = R"(
 	{
 		"language": "Solidity",
 		"settings": {
@@ -1373,12 +1340,38 @@ BOOST_AUTO_TEST_CASE(use_stack_optimization)
 		},
 		"sources": {
 			"fileA": {
-				"content":  )" + Json(contractCode).dump() + R"(
+				"content": "contract A {
+					function y() public {
+						assembly {
+							function fun() -> a3, b3, c3, d3, e3, f3, g3, h3, i3, j3, k3, l3, m3, n3, o3, p3
+							{
+								let a := 1
+								let b := 1
+								let z3 := 1
+								sstore(a, b)
+								sstore(add(a, 1), b)
+								sstore(add(a, 2), b)
+								sstore(add(a, 3), b)
+								sstore(add(a, 4), b)
+								sstore(add(a, 5), b)
+								sstore(add(a, 6), b)
+								sstore(add(a, 7), b)
+								sstore(add(a, 8), b)
+								sstore(add(a, 9), b)
+								sstore(add(a, 10), b)
+								sstore(add(a, 11), b)
+								sstore(add(a, 12), b)
+							}
+							let a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1, m1, n1, o1, p1 := fun()
+							let a2, b2, c2, d2, e2, f2, g2, h2, i2, j2, k2, l2, m2, n2, o2, p2 := fun()
+							sstore(a1, a2)
+						}
+					}
+				}"
 			}
 		}
 	}
 	)";
-	std::cout << input << std::endl;
 
 	Json parsedInput;
 	BOOST_REQUIRE(util::jsonParseStrict(input, parsedInput));
